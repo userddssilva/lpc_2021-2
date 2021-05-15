b17c6ee54e709a13e4ac01518b9b0457dba263ef
Dayvson Silva <userddssilva@gmail.com>
2021-05-15 19:24:17 -0400

    add: add somes constanst

--- pong/constants.py
+++ pong/constants.py
@@ -2,7 +2,10 @@ PLAY_BLEEP_SOUND = "aplay bleep_sound.wav&"
 PLAY_BOUNCE_SOUND = "aplay bounce_sound.wav&"
 
 SCREEN_WIDTH = 800
-SCREEN_WEIGHT = 600
+SCREEN_HEIGHT = 600
 
-stretch_wid = 5
-stretch_len = 1
+STRETCH_WID = 5
+STRETCH_LEN = 1
+
+PLAYER_1 = 'Player 1'
+PLAYER_2 = 'Player 2'
